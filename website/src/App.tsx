008948423c4d492d404ba5c98dce7100cf0ad2f6
Matteo McGuinness <mcguinm4@tcd.ie>
2021-12-20 22:54:22 +0000

    viewBasicUserDataInProfile

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -7,11 +7,13 @@ import "./styles/global.scss";
 /* ---------------------------------------------------------- PAGES ---------------------------------------------------------- */
 
 import HomePage from "./pages/HomePage";
+import UserProfile from "./pages/UserProfile";
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/user/:username" element={<UserProfile />} />
         <Route path="/" element={<HomePage />} />
       </Routes>
     </Router>
---
+++ website/src/pages/UserProfile.tsx
@@ -0,0 +1,55 @@
+/* THIRD PARTY FUNCTIONS */
+import React, { useState, useEffect, useRef } from "react";
+import { useParams } from "react-router-dom";
+
+/* API */
+import { connect } from "react-redux";
+import { getBasicUserInfo } from "../actions/usersActions";
+
+function UserProfile(props: any) {
+  let { username } = useParams();
+
+  const [userData, setUserData]: any = useState({});
+
+  useEffect(() => {
+    if (
+      props.userBasicInfo &&
+      props.userBasicInfo.data &&
+      props.userBasicInfo.data.login === username
+    ) {
+      setUserData(props.userBasicInfo.data);
+    } else {
+      props.getBasicUserInfo(username);
+    }
+  }, [username]);
+
+  const firstUpdate = useRef(true);
+  useEffect(() => {
+    if (firstUpdate.current) {
+      firstUpdate.current = false;
+      return;
+    }
+    if (props.userBasicInfo.data.error) {
+      alert(props.userBasicInfo.data.error);
+    } else if (props.userBasicInfo.data) {
+      setUserData(props.userBasicInfo.data);
+    }
+  }, [props.userBasicInfo]);
+
+  return (
+    <>
+      <div>UserProfile {userData.login}</div>
+      <img src={userData.avatar_url} alt={userData.name} />
+      <p>{userData.name}</p>
+      <p>{userData.bio}</p>
+    </>
+  );
+}
+
+const mapStateToProps = (state: any) => ({
+  userBasicInfo: state.users.userBasicInfo,
+});
+
+export default connect(mapStateToProps, {
+  getBasicUserInfo,
+})(UserProfile);
